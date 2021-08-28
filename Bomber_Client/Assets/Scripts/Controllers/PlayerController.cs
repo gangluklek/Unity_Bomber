609b55ac1e426cb3b2c129a30a3cf2f74067230f
Voratep L <70006753@krungsri.com>
2021-08-28 22:27:59 +0700

    [Go][Week2] InitDataModel &CreatePlayerModel

---
+++ Bomber_Client/Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private CharacterController characterController;
+    private int id;
+    private Client client;
+
+    public int Id => id;
+
+    internal void Setup(CreatePlayerModel model, Client client)
+    {
+        id = model.Id;
+        transform.position = model.Position;
+        this.client = client;
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
